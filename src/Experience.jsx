8d10f002f7309f3e7db9af926ee318bb46cb710e
Xavier Georget <xageorget@gmail.com>
2023-07-27 17:48:33 +0200

    OrbitControls, TransformControls and PivotControls from drei

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,53 +1,39 @@
+import {
+  OrbitControls,
+  PivotControls,
+  TransformControls,
+} from "@react-three/drei";
 import { useRef } from "react";
-import { extend, useFrame, useThree } from "@react-three/fiber";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import CustomObject from "./components/CustomObject";
-
-extend({ OrbitControls });
-
 export default function Experience() {
-  const { camera, gl } = useThree();
   const cubeRef = useRef();
-  const groupRef = useRef();
-
-  useFrame((state, delta) => {
-    // const angle = state.clock.elapsedTime;
-    // state.camera.position.x = Math.sin(angle) * 8;
-    // state.camera.position.z = Math.cos(angle) * 8;
-    // state.camera.lookAt(0, 0, 0);
-
-    cubeRef.current.rotation.y += delta;
-    // groupRef.current.rotation.y += delta;
-  });
-
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
-
+      <OrbitControls makeDefault />
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
-
-      <group ref={groupRef}>
-        <mesh
-          ref={cubeRef}
-          position-x={2}
-          rotation-y={Math.PI * 0.25}
-          scale={1.5}
-        >
-          <boxGeometry />
-          <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+      <mesh position-x={2} scale={1.5} ref={cubeRef}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+      <TransformControls object={cubeRef} mode="translate" />
+      <PivotControls
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        lineWidth={2}
+        axisColors={["#9381ff", "#ff4d6d", "#7ae582"]}
+        scale={100}
+        fixed={true}
+      >
         <mesh position-x={-2}>
           <sphereGeometry />
           <meshStandardMaterial color="orange" />
         </mesh>
-      </group>
+      </PivotControls>
+
       <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
-
-      <CustomObject />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,13 @@ import "./index.css";
 
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
-import {
-  CineonToneMapping,
-  ACESFilmicToneMapping,
-  SRGBColorSpace,
-} from "three";
+import { ACESFilmicToneMapping, SRGBColorSpace } from "three";
 
 const cameraSettings = {
   fov: 45,
-  // zoom: 100,
   near: 0.1,
   far: 200,
-  position: [3, 2, 6],
+  position: [-4, 3, 6],
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(
