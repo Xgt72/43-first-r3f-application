18e01fb76f988a2dfd374ac45c71dafb12849a3e
Xavier Georget <xageorget@gmail.com>
2023-07-27 12:24:18 +0200

    some configuration in Canvas

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,30 @@ import "./index.css";
 
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
+import {
+  CineonToneMapping,
+  ACESFilmicToneMapping,
+  SRGBColorSpace,
+} from "three";
 
 const cameraSettings = {
   fov: 45,
-  zoom: 100,
+  // zoom: 100,
   near: 0.1,
   far: 200,
   position: [3, 2, 6],
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Canvas orthographic camera={cameraSettings}>
+  <Canvas
+    gl={{
+      antialias: true,
+      toneMapping: ACESFilmicToneMapping,
+      outputColorSpace: SRGBColorSpace,
+    }}
+    orthographic={false}
+    camera={cameraSettings}
+  >
     <Experience />
   </Canvas>
 );
