cad0ecbf69b814dc094084de013435cffc43401c
Xgt72 <xageorget@gmail.com>
2023-07-30 19:03:32 +0200

    useHelpers

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,53 +1,48 @@
+import { useFrame } from "@react-three/fiber";
+import { useHelper, OrbitControls } from "@react-three/drei";
 import { useRef } from "react";
-import { extend, useFrame, useThree } from "@react-three/fiber";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import CustomObject from "./components/CustomObject";
-
-extend({ OrbitControls });
+import { Perf } from "r3f-perf";
+import * as THREE from "three";
 
 export default function Experience() {
-  const { camera, gl } = useThree();
-  const cubeRef = useRef();
-  const groupRef = useRef();
+  const directionalLightRef = useRef();
+  useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1);
+
+  const cube = useRef();
 
   useFrame((state, delta) => {
-    // const angle = state.clock.elapsedTime;
-    // state.camera.position.x = Math.sin(angle) * 8;
-    // state.camera.position.z = Math.cos(angle) * 8;
-    // state.camera.lookAt(0, 0, 0);
-
-    cubeRef.current.rotation.y += delta;
-    // groupRef.current.rotation.y += delta;
+    cube.current.rotation.y += delta * 0.2;
   });
 
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
+      <color args={["ivory"]} attach="background" />
 
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <Perf position="top-left" />
+
+      <OrbitControls makeDefault />
+
+      <directionalLight
+        ref={directionalLightRef}
+        position={[1, 2, 3]}
+        intensity={1.5}
+      />
       <ambientLight intensity={0.5} />
 
-      <group ref={groupRef}>
-        <mesh
-          ref={cubeRef}
-          position-x={2}
-          rotation-y={Math.PI * 0.25}
-          scale={1.5}
-        >
-          <boxGeometry />
-          <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-        <mesh position-x={-2}>
-          <sphereGeometry />
-          <meshStandardMaterial color="orange" />
-        </mesh>
-      </group>
-      <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
+      <mesh position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh ref={cube} position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
-
-      <CustomObject />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,28 @@
-import ReactDOM from "react-dom/client";
 import "./index.css";
-
+import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
 import Experience from "./Experience";
-import {
-  CineonToneMapping,
-  ACESFilmicToneMapping,
-  SRGBColorSpace,
-} from "three";
 
 const cameraSettings = {
   fov: 45,
-  // zoom: 100,
   near: 0.1,
   far: 200,
-  position: [3, 2, 6],
+  position: [-4, 3, 6],
 };
+
+// const created = ({ gl }) => {
+//   gl.setClearColor("#ff0000", 1);
+// };
+
+// const created = ({ scene }) => {
+//   scene.background = new THREE.Color("purple");
+// };
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Canvas
-    gl={{
-      antialias: true,
-      toneMapping: ACESFilmicToneMapping,
-      outputColorSpace: SRGBColorSpace,
-    }}
-    orthographic={false}
     camera={cameraSettings}
+    // onCreated={created}
   >
     <Experience />
   </Canvas>
