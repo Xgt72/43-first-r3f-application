65d4dd3c51f933c78af5ea8f8ea418aeef8e338a
Xgt72 <xageorget@gmail.com>
2023-07-31 16:05:44 +0200

    init

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,53 +1,40 @@
 import { useRef } from "react";
-import { extend, useFrame, useThree } from "@react-three/fiber";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import CustomObject from "./components/CustomObject";
-
-extend({ OrbitControls });
+import { OrbitControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { Perf } from "r3f-perf";
 
 export default function Experience() {
-  const { camera, gl } = useThree();
   const cubeRef = useRef();
-  const groupRef = useRef();
 
   useFrame((state, delta) => {
-    // const angle = state.clock.elapsedTime;
-    // state.camera.position.x = Math.sin(angle) * 8;
-    // state.camera.position.z = Math.cos(angle) * 8;
-    // state.camera.lookAt(0, 0, 0);
-
-    cubeRef.current.rotation.y += delta;
-    // groupRef.current.rotation.y += delta;
+    cubeRef.current.rotation.y += delta * 0.2;
   });
 
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
-
+      <Perf position="top-left" />
+      <OrbitControls makeDefault />
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <group ref={groupRef}>
-        <mesh
-          ref={cubeRef}
-          position-x={2}
-          rotation-y={Math.PI * 0.25}
-          scale={1.5}
-        >
-          <boxGeometry />
-          <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-        <mesh position-x={-2}>
-          <sphereGeometry />
-          <meshStandardMaterial color="orange" />
-        </mesh>
-      </group>
+      <mesh
+        ref={cubeRef}
+        position-x={2}
+        rotation-y={Math.PI * 0.25}
+        scale={1.5}
+      >
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+      <mesh position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
       <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
-
-      <CustomObject />
     </>
   );
 }
