a75c3163838060bb929b3174c6a04eba5b6b6345
Xgt72 <xageorget@gmail.com>
2023-08-02 16:20:46 +0200

    pointer events with R3F

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,53 +1,92 @@
-import { useRef } from "react";
-import { extend, useFrame, useThree } from "@react-three/fiber";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import CustomObject from "./components/CustomObject";
-
-extend({ OrbitControls });
+import { useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import {
+  useCursor,
+  useGLTF,
+  meshBounds,
+  OrbitControls,
+} from "@react-three/drei";
+import * as THREE from "three";
 
 export default function Experience() {
-  const { camera, gl } = useThree();
+  const [cubeHovered, setCubeHovered] = useState(false);
   const cubeRef = useRef();
-  const groupRef = useRef();
 
   useFrame((state, delta) => {
-    // const angle = state.clock.elapsedTime;
-    // state.camera.position.x = Math.sin(angle) * 8;
-    // state.camera.position.z = Math.cos(angle) * 8;
-    // state.camera.lookAt(0, 0, 0);
-
-    cubeRef.current.rotation.y += delta;
-    // groupRef.current.rotation.y += delta;
+    cubeRef.current.rotation.y += delta * 0.2;
   });
+
+  const eventHandler = (event) => {
+    console.log("---");
+    console.log("distance", event.distance); // Distance between camera and hit point
+    console.log("point", event.point); // Hit point coordinates (in 3D)
+    console.log("uv", event.uv); // UV coordinates on the geometry (in 2D)
+    console.log("object", event.object); // The object that triggered the event
+    console.log("eventObject", event.eventObject); // The object that was listening to the event (useful where there is objects in objects)
+
+    console.log("---");
+    console.log("x", event.x); // 2D screen coordinates of the pointer
+    console.log("y", event.y); // 2D screen coordinates of the pointer
+
+    console.log("---");
+    console.log("shiftKey", event.shiftKey); // If the SHIFT key was pressed
+    console.log("ctrlKey", event.ctrlKey); // If the CTRL key was pressed
+    console.log("metaKey", event.metaKey); // If the COMMAND key was pressed
+
+    cubeRef.current.material.color.set(
+      `hsl(${Math.random() * 360}, 100%, 75%)`
+    );
+  };
+
+  useCursor(cubeHovered);
+
+  const hamburgerModel = useGLTF("./hamburger-draco.glb");
 
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
+      <OrbitControls makeDefault />
 
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <group ref={groupRef}>
-        <mesh
-          ref={cubeRef}
-          position-x={2}
-          rotation-y={Math.PI * 0.25}
-          scale={1.5}
-        >
-          <boxGeometry />
-          <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-        <mesh position-x={-2}>
-          <sphereGeometry />
-          <meshStandardMaterial color="orange" />
-        </mesh>
-      </group>
+      <mesh
+        ref={cubeRef}
+        raycast={meshBounds}
+        position-x={2}
+        rotation-y={Math.PI * 0.25}
+        scale={1.5}
+        onClick={eventHandler}
+        onPointerEnter={() => setCubeHovered(true)}
+        onPointerLeave={() => setCubeHovered(false)}
+      >
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh
+        position-x={-2}
+        onClick={(event) => {
+          event.stopPropagation();
+        }}
+      >
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
       <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
 
-      <CustomObject />
+      <primitive
+        onClick={(e) => {
+          e.stopPropagation();
+          console.log(e.object.name);
+        }}
+        object={hamburgerModel.scene}
+        scale={0.25}
+        position-y={0.5}
+      />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,30 +3,16 @@ import "./index.css";
 
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
-import {
-  CineonToneMapping,
-  ACESFilmicToneMapping,
-  SRGBColorSpace,
-} from "three";
 
 const cameraSettings = {
   fov: 45,
-  // zoom: 100,
   near: 0.1,
   far: 200,
-  position: [3, 2, 6],
+  position: [-4, 3, 8],
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Canvas
-    gl={{
-      antialias: true,
-      toneMapping: ACESFilmicToneMapping,
-      outputColorSpace: SRGBColorSpace,
-    }}
-    orthographic={false}
-    camera={cameraSettings}
-  >
+  <Canvas camera={cameraSettings}>
     <Experience />
   </Canvas>
 );
