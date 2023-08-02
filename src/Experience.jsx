d07d0b2245eff3fa7119a6537314f66d46ebd528
Xgt72 <xageorget@gmail.com>
2023-08-02 14:30:23 +0200

    starter point

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,39 +1,13 @@
 import { useRef } from "react";
 import { OrbitControls } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import { Perf } from "r3f-perf";
 
 export default function Experience() {
-  const cubeRef = useRef();
-
-  useFrame((state, delta) => {
-    cubeRef.current.rotation.y += delta * 0.2;
-  });
-
   return (
     <>
-      <Perf position="top-left" />
       <OrbitControls makeDefault />
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
-      <ambientLight intensity={0.5} />
-
-      <mesh
-        ref={cubeRef}
-        position-x={2}
-        rotation-y={Math.PI * 0.25}
-        scale={1.5}
-      >
+      <mesh scale={1.5}>
         <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
-      </mesh>
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
-
-      <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
-        <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
+        <meshNormalMaterial />
       </mesh>
     </>
   );
