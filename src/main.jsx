ba8ad49cb41d53245a4c7d4585243ce39ed8283b
Xgt72 <xageorget@gmail.com>
2023-08-02 15:17:47 +0200

    import portal model with baked texture and portal shader

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ const cameraSettings = {
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Canvas camera={cameraSettings}>
+  <Canvas flat camera={cameraSettings}>
     <Experience />
   </Canvas>
 );
