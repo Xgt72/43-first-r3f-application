cd31a7bcd43aae43f334cee9bdda5d6acc064ba7
Xgt72 <xageorget@gmail.com>
2023-08-22 07:59:15 +0200

    react-three/rapier with colliders

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
@@ -8,11 +9,11 @@ const cameraSettings = {
   fov: 45,
   near: 0.1,
   far: 200,
-  position: [-4, 3, 6],
+  position: [4, 2, 6],
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Canvas camera={cameraSettings}>
+  <Canvas shadows camera={cameraSettings}>
     <Experience />
   </Canvas>
 );
